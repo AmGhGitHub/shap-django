536720c22acaeb2c50947aa96d796437a17a1491
Amir Ghaderi <altghaderi@gmail.com>
2022-02-24 16:04:42 -0700

    generated y values with shap values

--- api/util/ml_shap.py
+++ api/util/ml_shap.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import shap
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import r2_score
 from xgboost import XGBRegressor
 import json
@@ -16,9 +17,11 @@ def round_df(input_df, n_decimial):
     return df.applymap(lambda x: round(x, n_decimial))
 
 
-def get_shap_sample_values_arr(df):
-    n = df.shape[0]
-    return [np.c_[df[col].values, np.ones(n)+i].tolist() for i, col in enumerate(df.columns)]
+def get_shap_sample_values_arr(df_shap, y_scaled):
+    n = df_shap.shape[0]
+    ones=np.ones(n)
+    # rand=np.random.rand(n)
+    return [np.c_[df_shap[col].values, ones+i, y_scaled.reshape(-1,1)].tolist() for i, col in enumerate(df_shap.columns)]
 
 
 def generate_ml_and_shap_data(df_js):
@@ -27,6 +30,8 @@ def generate_ml_and_shap_data(df_js):
     output_column_name = 'output'
     X = df.loc[:, df.columns != output_column_name]
     y = df.iloc[:, df.columns == output_column_name]
+    # scaler = MinMaxScaler()
+    
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42)
 
@@ -55,10 +60,15 @@ def generate_ml_and_shap_data(df_js):
 
     df_train_shapValues_sample = round_df(
         df_train_shapValues.sample(frac=0.4, replace=False), 4)
+    
+    y_train_pred_scaled=y_train_pred/(max(y_train_pred)-min(y_train_pred))
+    
+    y_train_pred_scaled_smaple=y_train_pred_scaled[df_train_shapValues_sample.index.values]
+    
 
     shap_features = [col for col in df_train_shapValues.columns]
     shap_values_sample_arr = get_shap_sample_values_arr(
-        df_train_shapValues_sample)
+        df_train_shapValues_sample,y_train_pred_scaled_smaple)
     feature_importance = np.abs(shap_values).mean(0).tolist()
 
     return {"model": {"r2": {"train_data": model_r2_train_data, "test_data": model_r2_test_data},
